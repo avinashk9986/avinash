108ce2b4da893e1c634ee056c88d91f220db4be8
avinashk9986 <41304863+avinashk9986@users.noreply.github.com>
2018-11-05 01:23:32 +0530

    Delete createinstance.py

--- createinstance.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import boto3
-ec2 = boto3.resource('ec2')
-instance = ec2.create_instances(
-    ImageId='ami-0a1691801b49e0f28',
-    MinCount=1,
-    MaxCount=1,
-    InstanceType='t2.micro')
-print instance[0].id
